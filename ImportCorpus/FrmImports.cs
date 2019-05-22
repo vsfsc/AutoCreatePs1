da66e1d39b563250fccdf76dd2ce514d87751531
xueqingxia <962204414@qq.com>
2019-05-22 16:21:28 +0800

    imports

---
+++ ImportCorpus/FrmImports.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImportCorpus
+{
+    public partial class FrmImports : Form
+    {
+        public FrmImports()
+        {
+            InitializeComponent();
+        }
+    }
+}
