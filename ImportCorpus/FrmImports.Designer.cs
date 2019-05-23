26e776c3aefaca7c6e7ef8ff829ef21d83557fe3
xueqingxia <962204414@qq.com>
2019-05-23 15:28:08 +0800

    import

--- ImportCorpus/FrmImports.Designer.cs
+++ ImportCorpus/FrmImports.Designer.cs
@@ -28,20 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lbMsg = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rbAC = new System.Windows.Forms.RadioButton();
+            this.rbLC = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rbTagged = new System.Windows.Forms.RadioButton();
+            this.rbUntagged = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPath.Location = new System.Drawing.Point(121, 211);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(500, 29);
+            this.txtPath.TabIndex = 2;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnImport.Location = new System.Drawing.Point(366, 281);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(110, 31);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "导入";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(118, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(389, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "填写或通过按钮打开语料文件所在的文件夹：";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOpen.Location = new System.Drawing.Point(628, 212);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(108, 30);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "打开文件夹";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lbMsg
+            // 
+            this.lbMsg.AutoSize = true;
+            this.lbMsg.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbMsg.ForeColor = System.Drawing.Color.Maroon;
+            this.lbMsg.Location = new System.Drawing.Point(162, 260);
+            this.lbMsg.Name = "lbMsg";
+            this.lbMsg.Size = new System.Drawing.Size(57, 12);
+            this.lbMsg.TabIndex = 6;
+            this.lbMsg.Text = "信息提醒";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(129, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "语料库类型：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(129, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "是否赋码：";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.rbAC);
+            this.panel1.Controls.Add(this.rbLC);
+            this.panel1.Location = new System.Drawing.Point(275, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(242, 55);
+            this.panel1.TabIndex = 11;
+            // 
+            // rbAC
+            // 
+            this.rbAC.AutoSize = true;
+            this.rbAC.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rbAC.Location = new System.Drawing.Point(162, 22);
+            this.rbAC.Name = "rbAC";
+            this.rbAC.Size = new System.Drawing.Size(77, 23);
+            this.rbAC.TabIndex = 3;
+            this.rbAC.Text = "NEUAC";
+            this.rbAC.UseVisualStyleBackColor = true;
+            // 
+            // rbLC
+            // 
+            this.rbLC.AutoSize = true;
+            this.rbLC.Checked = true;
+            this.rbLC.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rbLC.Location = new System.Drawing.Point(4, 22);
+            this.rbLC.Name = "rbLC";
+            this.rbLC.Size = new System.Drawing.Size(77, 23);
+            this.rbLC.TabIndex = 2;
+            this.rbLC.TabStop = true;
+            this.rbLC.Text = "NEULC";
+            this.rbLC.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.rbTagged);
+            this.panel2.Controls.Add(this.rbUntagged);
+            this.panel2.Location = new System.Drawing.Point(275, 127);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(254, 51);
+            this.panel2.TabIndex = 12;
+            // 
+            // rbTagged
+            // 
+            this.rbTagged.AutoSize = true;
+            this.rbTagged.Checked = true;
+            this.rbTagged.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rbTagged.Location = new System.Drawing.Point(161, 10);
+            this.rbTagged.Name = "rbTagged";
+            this.rbTagged.Size = new System.Drawing.Size(87, 23);
+            this.rbTagged.TabIndex = 12;
+            this.rbTagged.TabStop = true;
+            this.rbTagged.Text = "Tagged";
+            this.rbTagged.UseVisualStyleBackColor = true;
+            // 
+            // rbUntagged
+            // 
+            this.rbUntagged.AutoSize = true;
+            this.rbUntagged.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rbUntagged.Location = new System.Drawing.Point(3, 10);
+            this.rbUntagged.Name = "rbUntagged";
+            this.rbUntagged.Size = new System.Drawing.Size(107, 23);
+            this.rbUntagged.TabIndex = 11;
+            this.rbUntagged.Text = "UnTagged";
+            this.rbUntagged.UseVisualStyleBackColor = true;
             // 
             // FrmImports
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(749, 407);
+            this.ClientSize = new System.Drawing.Size(874, 441);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbMsg);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.txtPath);
+            this.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Name = "FrmImports";
-            this.Text = "Form1";
+            this.Text = "批量导入语料库";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label lbMsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rbAC;
+        private System.Windows.Forms.RadioButton rbLC;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton rbTagged;
+        private System.Windows.Forms.RadioButton rbUntagged;
     }
 }
 
--- ImportCorpus/FrmImports.cs
+++ ImportCorpus/FrmImports.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,139 @@ namespace ImportCorpus
         public FrmImports()
         {
             InitializeComponent();
+            if (FSCDLL.DAL.DataProvider.ConnectionString == "")
+                FSCDLL.DAL.DataProvider.ConnectionString = GetConnString("SqlProviderFSC");
+            if (dtCorpus == null)
+                dtCorpus = FSCDLL.DAL.Corpus.GetCorpus().Tables[0];
+            lbMsg.Text = "";
         }
+        //打开文件夹
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            txtPath.Text = ChooseFolderPath();
+        }
+        #region 
+        /// <summary>
+        /// 对话框形式选择文件夹路径
+        /// </summary>
+        /// <returns>返回所选择的文件夹路径</returns>
+        public string ChooseFolderPath()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            fbd.Description = "请选择目录";
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                return fbd.SelectedPath.ToString();
+            }
+            else
+            {
+                return "";
+
+            }
+        }
+        #endregion
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (txtPath.Text == "")
+            {
+                lbMsg.Text = "文件夹不能为空！";
+                return;
+            }
+            string txt = Text;
+            Text="正在导入……";
+            int total = 0;
+            StreamWriter w = new StreamWriter(@"c:\importCorpus.txt", true);
+            DirectoryInfo dir = new DirectoryInfo(txtPath.Text);
+            DealFiles(dir, ref total);
+            foreach (DirectoryInfo subDir in dir.GetDirectories())
+            {
+                DealFiles(subDir, ref total);
+            }
+            w.Write(DateTime.Now.ToString ()+"导入文件夹："+txtPath.Text );
+            w.WriteLine(";本次导入条："+ total );
+            w.Close();
+            Text = txt;
+            MessageBox.Show ( "导入完成！");
+        }
+        private void DealFiles(DirectoryInfo dir,ref int total)
+        {
+            //遍历文件夹的文件
+            foreach (FileInfo file in dir.GetFiles("*.txt"))
+            {
+                string fileName = file.Name;//.Replace(file.Extension, "");//去掉文件扩展名
+                //处理非数字部分
+                string ignore = "[.a-zA-Z_-]";//
+                fileName = Regex.Replace(fileName  , ignore, "");
+                string fileContent;
+                 if (rbLC.Checked && fileName.Length == 8)
+                {
+                    string gradeIDs = split + fileName.Substring(0, 1) + split;
+                    string genreIDs = split + fileName.Substring(1, 1) + split;
+                    string topicIDs = split + fileName.Substring(2, 1) + split;
+                    StreamReader read = file.OpenText();
+                    fileContent = read.ReadToEnd().Trim();
+                    Save(fileName, fileContent, topicIDs, genreIDs, gradeIDs);
+                    total += 1;
+                }
+            }
+        }
+       
+        //数据分隔符，用来分隔外键的ID值
+        private const string split = ";";
+        private static DataTable dtCorpus;
+
+        private void Save(string title, string corpusText, string topicIds, string genreIds, string gradeIds)
+        {
+            DataRow dr = dtCorpus.NewRow();
+            long corpusID = 0;
+            //更改为标题唯一
+            DataRow[] drs = dtCorpus.Select("Title='" + title + "'");// and OriginalText='" + corpusText + "'");//通过标题和正文判断唯一性
+            if (drs.Length > 0)
+            {
+                dr = drs[0];
+                corpusID = Convert.ToInt64(dr["CorpusID"].ToString());
+            }
+            else
+            {
+                dr["Created"] = DateTime.Now;
+                dr["Author"] = 0;
+                dr["Flag"] = 1;
+                dr["Title"] = title;
+                dr["TopicID"] = topicIds;
+                dr["GenreID"] = genreIds;
+                dr["GradeID"] = gradeIds;
+                //用来保存语料库的类别
+                dr["Source"] = rbLC.Checked ? rbLC.Text : rbAC.Text;
+
+            }
+            if (rbUntagged.Checked)
+                dr["OriginalText"] = corpusText;
+            else
+                dr["CodedText"] = corpusText;
+            if (corpusID == 0)
+            {
+                FSCDLL.DAL.Corpus.InsertCorpus(null, dr);
+                dtCorpus.Rows.Add(dr.ItemArray);
+            }
+
+            else
+            {
+                FSCDLL.DAL.Corpus.UpdateCorpus(null, dr);
+                dtCorpus.AcceptChanges();
+            }
+        }
+        /// <summary>
+        /// 获取配置文件中的连接字符串
+        /// </summary>
+        /// <param name="connStrName">字符串的名称</param>
+        /// <returns></returns>
+        public string GetConnString(string connStrName)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[connStrName].ConnectionString;
+            return connectionString;
+        }
+
     }
 }
